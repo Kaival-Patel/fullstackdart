14a70763fe60c0a56a4ea507803ab903396f9ec3
Kaival Patel <kaivalpatel53@gmail.com>
2022-07-26 22:53:27 +0530

    Managed SQL in dart

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
-
 import 'package:hotreloader/hotreloader.dart';
+import 'package:mysql1/mysql1.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -9,6 +12,7 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 
 // Configure routes.
 int counter = 0;
+late Socket socket;
 List<WebSocketChannel> clients = [];
 final _router = Router()
   ..get('/', _rootHandler)
@@ -40,6 +44,7 @@ Response _echoHandler(Request request) {
 }
 
 void main(List<String> args) async {
+  //dart --enable-vm-service bin/server.dart
   // Use any available host or container IP (usually `0.0.0.0`).
   final reloader = await HotReloader.create();
   final ip = InternetAddress.anyIPv4;
@@ -47,10 +52,45 @@ void main(List<String> args) async {
   // Configure a pipeline that logs requests.
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
+  //SQL CONNECTION
+  try {
+    // socket = await Socket.connect('localhost', 3306);
+    // print("Connected SQL Server on ${socket.port}");
+    // String json = jsonEncode({
+    //   "type": "open",
+    //   "text":
+    //       "Server=localhost\\SQLEXPRESS;Database=Test123;Trusted_Connection=yes;"
+    // });
+    // // String json = jsonEncode({"type": "table", "text": "SELECT * FROM Test"});
+    // _sendCommand(json).then((result) {
+    //   print("DB RESULT => $result");
+    // }).catchError((err) {
+    //   print('Querying Error');
+    // });
+    var settings = ConnectionSettings(
+        host: 'localhost', port: 3306, user: 'root', db: 'test123');
+    var conn = await MySqlConnection.connect(settings);
+    print(await conn.query('SELECT * FROM tt'));
+  } catch (e) {
+    print(e);
+  }
+
   // For running in containers, we respect the PORT environment variable.
   //PORT 8080
   final port = int.parse('8080');
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
   reloader.stop();
+}
+
+Future<String> _sendCommand(String command) {
+  // prepare buffer for response
+  StringBuffer receiveBuffer = StringBuffer();
+
+  Completer<String> _completer = Completer();
+  String cmd = command.length.toString() + "\r\n" + command;
+  print(cmd);
+  socket.write(cmd);
+
+  return _completer.future;
 }
